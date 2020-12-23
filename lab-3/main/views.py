4a5802015377ebb0b99b7168717704e6215efddc
Toooolik <73467656+Toooolik@users.noreply.github.com>
2020-12-23 17:37:39 +0200

    Update views.py

--- lab-3/main/views.py
+++ lab-3/main/views.py
@@ -9,8 +9,5 @@ def main(request):
 
 
 def health(request):
-    response = dict(date= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()),
-                    current_page=request.get_full_path(),
-                    server_info=os.uname().release,
-                    client_info=os.getlogin())
+    response = dict(date= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), current_page=request.path, server_info=os.name, client_info=os.uname()[1])
     return JsonResponse(response)
